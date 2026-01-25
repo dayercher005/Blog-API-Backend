ef0c883ac21232424efff23f328fe1f631b91be9
Dayer <dayerxr.cher@gmail.com>
2026-01-25 23:42:14 +0800

    feat: added initial controllers for homepage and authentication forms

---
+++ src/controllers/dashboard.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from 'express';
+
+export function renderDashboard(req: Request, res: Response){
+
+}
---
+++ src/controllers/sign-up.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from 'express';
+
+export function renderSignUpForm(req: Request, res: Response){
+    
+}
+
+export function sendSignUpForm(req: Request, res: Response){
+    
+}
