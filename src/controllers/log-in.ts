f5b6b0e90ee8e6fe3adffa14297e562b81b784d1
Dayer <dayerxr.cher@gmail.com>
2026-01-26 17:06:41 +0800

    feat: initial implementation of generating jwt

--- src/controllers/log-in.ts
+++ src/controllers/log-in.ts
@@ -1,9 +1,42 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction, RequestHandler } from 'express';
+import { body, validationResult } from 'express-validator';
+import path from 'node:path';
+import fs from 'fs';
+import type { ValidationChain } from 'express-validator';
+import passport from 'passport';
+import { generateJWT } from '../config/jwtGenerator';
+import { ReadIndividualUser }  from '../lib/queries.ts';
+
+const pathToKey = path.join('src/config', 'id_rsa_priv.pem');
+const PrivateKey = fs.readFileSync(pathToKey, 'utf8');
+
+export const validateSignUpForm: (ValidationChain | RequestHandler)[] = [
+    body("username")
+    .notEmpty()
+    .isEmail()
+    .withMessage("Please enter a valid username"),
+    body("password")
+    .notEmpty()
+    .withMessage("Password cannot be empty"),
+    body("confirmPassword").custom((password:string , {req}) => {
+        return password === req.body.password
+    })
+]
 
 export function renderLogInForm(req: Request, res: Response){
-
+    res.json();
 }
 
-export function sendLogInForm(req: Request, res: Response){
+export async function sendLogInForm(req: Request, res: Response, next: NextFunction) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(404).json();
+    }
+    passport.authenticate("local", {session: false})
+    const individualUser = await ReadIndividualUser((req.user as any).username)
+    if (individualUser) {
+        const token = generateJWT(individualUser.id, individualUser.username);
+        res.json({token: token})
+    }
+}
     
-}
--- src/controllers/sign-up.ts
+++ src/controllers/sign-up.ts
@@ -1,9 +1,38 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, RequestHandler } from 'express';
+import { body, validationResult, matchedData } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import type { ValidationChain } from 'express-validator';
+import { CreateUsers } from '../lib/queries.ts';
+
+
+export const validateSignUpForm: (ValidationChain | RequestHandler)[] = [
+    body("username")
+    .notEmpty()
+    .isEmail()
+    .withMessage("Please enter a valid username"),
+    body("password")
+    .notEmpty()
+    .withMessage("Password cannot be empty"),
+    body("confirmPassword").custom((password:string , {req}) => {
+        return password === req.body.password
+    })
+]
 
 export function renderSignUpForm(req: Request, res: Response){
-    
+    res.json();
 }
 
-export function sendSignUpForm(req: Request, res: Response){
-    
+export async function sendSignUpForm(req: Request, res: Response){
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        res.status(400).json();
+    }
+
+    const { username, password } = matchedData(req);
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    CreateUsers(username, encryptedPassword);
+
+    res.redirect("/log-in");
 }
