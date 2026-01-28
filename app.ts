9244a042a747374f0cd49177a5bb50515ec3ba5c
Dayer <dayerxr.cher@gmail.com>
2026-01-28 23:14:09 +0800

    feat: added cors middleware

--- app.ts
+++ app.ts
@@ -1,18 +1,35 @@
 import express from 'express';
 import type { Application } from 'express';
 import cors from 'cors';
-import { LogInRouter } from './src/routes/log-in.ts';
-import { SignUpRouter } from './src/routes/sign-up.ts';
-import { DashboardRouter } from './src/routes/dashboard.ts';
+import { UserLogInRouter } from './src/routes/User/user-log-in.ts';
+import { UserSignUpRouter } from './src/routes/User/user-sign-up.ts';
+import { UserDashboardRouter } from './src/routes/User/user-dashboard.ts';
+
 
 const app: Application = express();
 
+const allowedOrigins: string[] = ['http://localhost:5173', 'http://localhost:5174'];
+
+interface DynamicCorsOptionsType {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => void;
+}
+
+const DynamicCorsOptions: DynamicCorsOptionsType = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1){
+      callback(null, true);
+    } else{
+      callback(new Error('Unauthorized origin'));
+    }
+  }
+}
 
 app.use(express.json());
+app.use(cors(DynamicCorsOptions));
 
-app.use("/", DashboardRouter);
-app.use("/log-in", LogInRouter);
-app.use("/sign-up", SignUpRouter);
+app.use("/", UserDashboardRouter);
+app.use("/log-in", UserLogInRouter);
+app.use("/sign-up", UserSignUpRouter);
 
 const PORT = 8080;
 app.listen(PORT, (error: any) => {
