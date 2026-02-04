84517a926a9b271cd39c321b3c9ff235f4668057
Dayer <dayerxr.cher@gmail.com>
2026-02-04 17:19:37 +0800

    chore: removed get route for comment form

--- src/routes/User-Frontend.ts
+++ src/routes/User-Frontend.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { renderDashboard, renderIndividualPost } from '../controllers/User/Homepage.ts';
-import { renderCommentForm, sendCommentForm } from '../controllers/User/CreateComment.ts';
+import { renderCommentForm, validateCommentForm, sendCommentForm } from '../controllers/User/CreateComment.ts';
 import { validateLogInForm, sendLogInForm } from '../controllers/User/Login.ts';
 import { validateSignUpForm, sendSignUpForm } from '../controllers/User/SignUp.ts';
 import "../config/jwtStrategy.ts";
@@ -11,7 +11,7 @@ export const UserFrontend = Router();
 UserFrontend.get("/", renderDashboard);
 UserFrontend.get("/:blogID", passport.authenticate("UserJwtStrategy", {session: false}), renderIndividualPost);
 UserFrontend.get("/:blogID/comment", passport.authenticate("UserJwtStrategy", {session: false}), renderCommentForm);
-UserFrontend.post("/:blogID/comment", passport.authenticate("UserJwtStrategy", {session: false}), sendCommentForm);
+UserFrontend.post("/:blogID/comment", passport.authenticate("UserJwtStrategy", {session: false}), validateCommentForm, sendCommentForm);
 
 UserFrontend.post("/log-in", validateLogInForm, sendLogInForm);
 
