b84bcb4d249a070d3d84944a9ae13dd5e128e99e
Dayer <dayerxr.cher@gmail.com>
2026-02-07 00:01:55 +0800

    feat: added homepage and dashboard routing in react router

--- src/routes/User-Frontend.ts
+++ src/routes/User-Frontend.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { renderDashboard, renderIndividualPost } from '../controllers/User/Homepage.ts';
+import { renderHomepage } from '../controllers/User/Homepage.ts';
+import { renderDashboard, renderIndividualPost, verifyUserStatus } from '../controllers/User/Dashboard.ts';
 import { renderCommentForm, validateCommentForm, sendCommentForm } from '../controllers/User/CreateComment.ts';
 import { validateLogInForm, sendLogInForm } from '../controllers/User/Login.ts';
 import { validateSignUpForm, sendSignUpForm } from '../controllers/User/SignUp.ts';
@@ -8,11 +9,39 @@ import passport from 'passport';
 
 export const UserFrontend = Router();
 
-UserFrontend.get("/", renderDashboard);
-UserFrontend.get("/:blogID", passport.authenticate("UserJwtStrategy", {session: false}), renderIndividualPost);
-UserFrontend.get("/:blogID/comment", passport.authenticate("UserJwtStrategy", {session: false}), renderCommentForm);
-UserFrontend.post("/:blogID/comment", passport.authenticate("UserJwtStrategy", {session: false}), validateCommentForm, sendCommentForm);
+UserFrontend.get("/", 
+    passport.authenticate("UserJwtStrategy", {session: false}),
+    renderHomepage
+);
 
-UserFrontend.post("/log-in", validateLogInForm, sendLogInForm);
+UserFrontend.get("/dashboard",
+    passport.authenticate("UserJwtStrategy", {session: false}), 
+    renderDashboard
+);
 
-UserFrontend.post("/sign-up", validateSignUpForm, sendSignUpForm);
+UserFrontend.get("/:blogID", 
+    passport.authenticate("UserJwtStrategy", {session: false}),
+    renderIndividualPost, 
+    verifyUserStatus
+);
+
+UserFrontend.get("/:blogID/comment", 
+    passport.authenticate("UserJwtStrategy", {session: false}), 
+    renderCommentForm
+);
+
+UserFrontend.post("/:blogID/comment", 
+    passport.authenticate("UserJwtStrategy", {session: false}), 
+    validateCommentForm, 
+    sendCommentForm
+);
+
+UserFrontend.post("/log-in", 
+    validateLogInForm, 
+    sendLogInForm
+);
+
+UserFrontend.post("/sign-up", 
+    validateSignUpForm, 
+    sendSignUpForm
+);
