9ffbea519d52f1fe61bec61559e90b008c793a4d
Dayer <dayerxr.cher@gmail.com>
2026-02-12 11:37:14 +0800

    chore: restructured routing for author frontend

--- src/routes/Author-Frontend.ts
+++ src/routes/Author-Frontend.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { renderDashboard } from '../controllers/Author/Dashboard.ts';
+import { renderHomepage } from '../controllers/Author/Homepage.ts';
+import { renderDashboard, renderIndividualBlog, renderIndividualComment, deleteIndividualComments } from '../controllers/Author/Dashboard.ts';
 import { renderCreateBlogForm, validatePostForm, sendPostForm } from '../controllers/Author/CreateBlog.ts';
 import { validateLogInForm, sendLogInForm } from '../controllers/Author/Login.ts';
 import { validateSignUpForm, sendSignUpForm } from '../controllers/Author/SignUp.ts';
@@ -8,10 +9,30 @@ import passport from 'passport';
 
 export const AuthorFrontend = Router();
 
+AuthorFrontend.get("/", 
+    passport.authenticate("AuthorJwtStrategy", {session: false}),
+    renderHomepage
+)
+
 AuthorFrontend.get("/dashboard", 
     passport.authenticate("AuthorJwtStrategy", {session: false}), 
     renderDashboard
 );
+
+AuthorFrontend.get("/dashboard/:blogID", 
+    passport.authenticate("AuthorJwtStrategy", {session: false}),
+    renderIndividualBlog
+)
+
+AuthorFrontend.get("/dashboard/:blogID/:commentID",
+    passport.authenticate("AuthorJwtStrategy", {session: false}),
+    renderIndividualComment
+)
+
+AuthorFrontend.delete("/dashboard/:blogID/:commentID",
+    passport.authenticate("AuthorJwtStrategy", {session: false}),
+    deleteIndividualComments
+)
 
 AuthorFrontend.get("/create-blog", 
     passport.authenticate("AuthorJwtStrategy", {session: false}), 
